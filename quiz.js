1308bcbb8a9d6c801aff20a6adcdb278a5b73f1a
Yann99999 <71888064+Yann99999@users.noreply.github.com>
2021-01-22 09:11:06 -0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,174 @@
+var pt =100;
+function pergunta1() {
+    let alt11 = document.getElementById("alt11").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt11 == true) {
+        document.getElementById("resp1").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp1").textContent = resultado1
+    }
+}
+function pergunta2() {
+    let alt14 = document.getElementById("alt14").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt14 == true) {
+        document.getElementById("resp2").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp2").textContent = resultado1
+    }
+}
+function pergunta3() {
+    let alt18 = document.getElementById("alt18").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt18 == true) {
+        document.getElementById("resp3").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp3").textContent = resultado1
+    }
+}
+function pergunta4() {
+    let alt22 = document.getElementById("alt22").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt22 == true) {
+        document.getElementById("resp4").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp4").textContent = resultado1
+    }
+}
+function pergunta5() {
+    let alt24 = document.getElementById("alt24").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt24 == true) {
+        document.getElementById("resp5").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp5").textContent = resultado1
+    }
+}
+function pergunta6() {
+    let alt26 = document.getElementById("alt26").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt26 == true) {
+        document.getElementById("resp6").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp6").textContent = resultado1
+    }
+}
+function pergunta7() {
+    let alt29 = document.getElementById("alt29").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt29 == true) {
+        document.getElementById("resp7").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp7").textContent = resultado1
+    }
+}
+function pergunta8() {
+    let alt34 = document.getElementById("alt34").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt34 == true) {
+        document.getElementById("resp8").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp8").textContent = resultado1
+    }
+}
+function pergunta9() {
+    let alt36 = document.getElementById("alt36").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt36 == true) {
+        document.getElementById("resp9").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp9").textContent = resultado1
+    }
+}
+function pergunta10() {
+    let alt40 = document.getElementById("alt40").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt40 == true) {
+        document.getElementById("resp10").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp10").textContent = resultado1
+    }
+}
+function pergunta11() {
+    let alt43 = document.getElementById("alt43").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt43 == true) {
+        document.getElementById("resp11").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp11").textContent = resultado1
+    }
+}
+function pergunta12() {
+    let alt45 = document.getElementById("alt45").checked
+    let resultado = "Você é um monstro sagrado, garotinho"
+    let resultado1 = "Você é um baita dum pé de rato, seu Oreiudo"
+
+    if (alt45 == true) {
+        document.getElementById("resp12").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp12").textContent = resultado1
+    }
+}
+function reset() {
+    document.getElementById("result").textContent = pt
+    pt = pt + 100
+    
+}
